3c79338810fb847c4c3077d1afac330cd2cfb468
Yus Kiem <yuskiem@icloud.com>
2022-09-13 06:21:57 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdlio>
+/**
+  *main function (always returns 0)
+  */
+int main (void)
+{
+	/*declaration of variables*/
+	string s;
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#Nested loops are basically advanced version of loops.
+#this is where we have loops inside loops.
