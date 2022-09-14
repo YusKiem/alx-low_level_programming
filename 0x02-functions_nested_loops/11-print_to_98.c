c641d1092e56a2f34e267a454963cc452ffb7034
Yus Kiem <yuskiem@icloud.com>
2022-09-14 10:19:04 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,11 @@
 void print_to_98(int n)
 {
 	if (n == 98)
-		printf("%d, \n", n);
+		_putchar('n'+'.');
 	else
-		while (n < 98)
+		while (n <= 98)
 		{
-			printf("%d, \n", n);
+			_putchar('n'+','+' ');
 			n++;
 		}
 }
