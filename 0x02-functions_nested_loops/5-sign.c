08a74966be30c05db79e80e3effed1e19d3611a7
Yus Kiem <yuskiem@icloud.com>
2022-09-14 04:16:11 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_sign - check the code for main.h.
+ *@n: letter being tested
+ * Return: Always 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
