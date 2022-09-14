548c14be0fd6e2308d8c7f99c963de273a999a8b
Yus Kiem <yuskiem@icloud.com>
2022-09-14 08:30:20 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * print_to_98 - prints natural numbers upto 98
+  *
+  *@n: natural number entered 
+  */
+void print_to_98(int n)
+{
+	while(n < 98)
+	{
+		printf("%d, \n", n);
+		n++;
+	}
+}
