c182daacb610726de78c9960255ef093fc6b20f6
Yus Kiem <yuskiem@icloud.com>
2022-09-13 18:58:02 -0700

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,10 +4,10 @@
   *
   *Return - it's a void function
  */
-void print_alphabet_x10 (void)
+void print_alphabet_x10(void)
 {
 	char c;
-	int i, n = 10 ;
+	int i, n = 10;
 
 	for (i = 0; i < n ; i++)
 	{
@@ -19,5 +19,4 @@ void print_alphabet_x10 (void)
 		}
 		_putchar('\n');
 	}
-	
 }
