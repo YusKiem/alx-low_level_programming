d8d05fca4035da15401317a83194d3be767a2b23
Yus Kiem <yuskiem@icloud.com>
2022-09-13 19:00:54 -0700

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
-  * main - prints lower case alphabets
+  * print_alphabet_x10 -  prints the alphabet x10
   *
-  *Return - it's a void function
+  * Return - it's a void function
+  *
  */
 void print_alphabet_x10(void)
 {
