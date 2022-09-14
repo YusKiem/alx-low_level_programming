f55ab820d99571f9d2ba8a0cd023b20e8deeec87
Yus Kiem <yuskiem@icloud.com>
2022-09-14 05:12:26 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * _abs - returns an absolute value of an integer
+  *
+  * Return: Always 0
+  */
+int _abs(void)
+{
+	if (c < 0)
+		printf("%d\n", c);
+	else
+		printf("%d\n", c);
+}
