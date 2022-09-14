ddf88d11d7912848fa8db573a324a0e60eb5466e
Yus Kiem <yuskiem@icloud.com>
2022-09-14 10:03:47 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,13 +3,16 @@
 /**
   * print_to_98 - prints natural numbers upto 98
   *
-  *@n: natural number entered 
+  *@n: natural number entered
   */
 void print_to_98(int n)
 {
-	while(n < 98)
-	{
+	if (n == 98)
 		printf("%d, \n", n);
-		n++;
-	}
+	else
+		while (n < 98)
+		{
+			printf("%d, \n", n);
+			n++;
+		}
 }
