27984f863b930c9d97411627f799172da042154b
Yus Kiem <yuskiem@icloud.com>
2022-09-13 19:50:55 -0700

    is allpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - check the code for Holberton School students.
+ *@c: letter being tested
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+	if (c == 224)
+		return (1);
+	else
+		return (0);
+}
