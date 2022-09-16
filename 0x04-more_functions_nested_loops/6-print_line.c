15a5d68e4c355db6b820ab1868c768257a86ebac
Yus Kiem <yuskiem@icloud.com>
2022-09-16 14:44:35 -0700

    pline

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,11 +10,11 @@ void print_line(int n)
 {
 	int i;
 	if (n <= 0)
-		putchar('\n');
+		_putchar('\n');
 	else
 	{
 		for (i = 0; i < n; i++)
-			putchar('_');
-		putchar('\n');
+			_putchar('_');
+		_putchar('\n');
 	}
 }
