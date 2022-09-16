d06322145d8d85f3079262583a235e0c654eb9cb
Yus Kiem <yuskiem@icloud.com>
2022-09-16 16:50:01 -0700

    diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,20 +7,15 @@
 
 void print_diagonal(int n)
 {
-	int i;
-	int j;
+	int i, j;
 
-	if (n <= 0)
-		putchar('\n');
-	else
+	for (i = 1; i <= n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			for (j = 1; j < i; j++)
-				putchar(' ');
-			putchar('\\');
-			putchar('\n');
-		}
+		for (j = 1; j < i; j++)
+			putchar(' ');
+		putchar('\\');
+		putchar('\n');
 	}
-	putchar('\n');
+	if (n <= 1)
+		putchar('\n');
 }
