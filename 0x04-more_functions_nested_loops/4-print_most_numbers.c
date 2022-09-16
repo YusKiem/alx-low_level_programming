235563a572eb720fffa5aac60d68768f67b27f9e
Yus Kiem <yuskiem@icloud.com>
2022-09-16 12:35:00 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
-  * print_most_numbers - prints all 0-9 except 2 and 4
+  * print_most_numbers - prints most numbers
   */
+
 void print_most_numbers(void)
 {
 	char i;
-	
+
 	while (i <= '9')
-		if ( i == '2' || i == '4')
+		if (i == '2' || i == '4')
 			i++;
 		else
 		{
