2e66cd4e7bf96c6ef2faba5d46c693e7ce784526
Yus Kiem <yuskiem@icloud.com>
2022-09-16 14:12:17 -0700

    prints a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+  * print_line - prints a straight line
+  * @n: length of the line
+  */
+
+void print_line(int n)
+{
+	if (n == 0)
+		putchar('\n');
+	else
+	{
+		for (int i = 0; i < n; i++)
+			putchar('_');
+		putchar('\n');
+	}
+}
