d7f489dbf70b006f71426614ffa2ab534d0da74f
Yus Kiem <yuskiem@icloud.com>
2022-09-19 15:00:57 -0700

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * _strlen - it computes the length of a given string
+  *
+  * @s: string whose length is to be computed for
+  * Return: length of the string s
+  */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,11 @@
 #ifndef _MAIN_H
 #define  _MAIN_H
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif
