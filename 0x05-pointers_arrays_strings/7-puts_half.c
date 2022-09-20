b27f3fed2a1b97069952030783ca88251009fb32
Yus Kiem <yuskiem@icloud.com>
2022-09-20 13:37:41 -0700

    wining

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,12 +15,12 @@ void puts_half(char *str)
 	if ((ct % 2) == 0)
 	{
 		for (even = ct / 2; str[even] != '\0'; even++)
-			_putchar(str[even]);
+			putchar(str[even]);
 	}
 	else
 	{
 		for (neg = (ct - 1) / 2; str[neg] != '\0'; neg++)
-			_putchar(str[neg + 1]);
+			putchar(str[neg + 1]);
 	}
 	_putchar('\n');
 }
