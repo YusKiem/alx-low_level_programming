3d94c156ee9ec40e9c7f4caaf805bead474015f5
Yus Kiem <yuskiem@icloud.com>
2022-09-21 13:24:48 -0700

    string cat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,12 +13,12 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
-	while ( dest[i] != '\0')
+	while (dest[i] != '\0')
 		i++;
 	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
 	}
-	return dest;
+	return (dest);
 }
