7d1cd89a48af6dc9e4e517430fb567db56235e18
Yus Kiem <yuskiem@icloud.com>
2022-09-21 03:02:15 -0700

    A function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <time.h>
+
+char *_strcat(char *dest, char *src);
+
+#endif
