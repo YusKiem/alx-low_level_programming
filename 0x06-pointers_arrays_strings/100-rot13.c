ba7b56d1455971426697d00a3cd57345bfbafabc
Yus Kiem <yuskiem@icloud.com>
2022-09-22 10:38:37 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,13 +9,21 @@
 char *rot13(char *str)
 {
 	int i = 0, j;
-	char alphs[52] = {'A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R','S', 'T', 'U', 'V', 'W', 'X','Y', 'Z', 'a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S','T', 'U', 'V', 'W', 'X', 'Y','T', 'U', 'V', 'W', 'X', 'Y','F', 'G', 'H', 'I', 'J', 'K','L', 'M', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w','x', 'y', 'z', 'a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i','j', 'k', 'l', 'm'};
+	char alphs[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		'X', 'Y', 'T', 'U', 'V', 'W', 'X', 'Y', 'F', 'G',
+		'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a',
+		'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
 	while (str[i])
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if(str[i] == alphs[j])
+			if (str[i] == alphs[j])
 			{
 				str[i] = rot13key[j];
 				break;
