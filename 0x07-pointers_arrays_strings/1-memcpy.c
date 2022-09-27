77ee0ffc22297500330a4bada5d9fb2ccd18e90d
Yus Kiem <yuskiem@icloud.com>
2022-09-27 06:03:28 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while (*src != '\0' && i <= n)
+	while (*src != '\0' && i < n)
 	{
 		*(dest + i) = *(src + i);
 		i++;
