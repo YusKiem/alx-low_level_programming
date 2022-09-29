c1493c5b4863ffcc7a2746a5bed4df97e5c22a5f
Yus Kiem <yuskiem@icloud.com>
2022-09-29 08:21:08 -0700

    recursion is a concursion

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Back with recursive functions
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <string.h>
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+#endif
