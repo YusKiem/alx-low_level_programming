97a2de7f1f3c0d2350418b53ea4b6a4e4f3940cb
Yus Kiem <yuskiem@icloud.com>
2022-09-29 13:51:25 -0700

    recursive factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,13 +7,13 @@
   * Return: integer, factorial
   */
 
-int factorial( int n)
+int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 0 )
+	else if (n == 0)
 		return (1);
-	else if (n ==1)
+	else if (n == 1)
 		return (1);
 	else
 		return (n * factorial(n - 1));
