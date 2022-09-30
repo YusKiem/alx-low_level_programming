c8cb13310f29bd00301696768064aa17aef70361
Yus Kiem <yuskiem@icloud.com>
2022-09-30 12:59:14 -0700

    prog name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * main - returns name of the argument
+  * @argc: argument counter
+  * @argv: argument vector
+  * Return: zero
+  */
+
+int main(int argc, char* argv[])
+{
+	int x = 0;
+
+	x = x + argc;
+	printf("%s\n", argv[0]);
+	return (x);
+}
