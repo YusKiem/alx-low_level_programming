66c12faabbb4442662a2cbd50dd238ef884439e9
Yus Kiem <yuskiem@icloud.com>
2022-10-05 17:53:11 -0700

    malloc 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,28 +2,27 @@
 
 /**
    *create_array - function to create an array of characters
-    *
-     * @c: character to fill array
-      * @size: size of the array
-       *
-        *Return: s(created array)
-	 */
+   *
+   * @c: character to fill arra
+   * @size: size of the array
+   *
+   *Return: s(created array
+ */
 
 char *create_array(unsigned int size, char c)
 {
-		char *s;
-			unsigned int i = 0;
+	char *s;
+	unsigned int i = 0;
 
-				if (size == 0)
-							return (NULL);
-					s = malloc(size * sizeof(char));
-
-						if (s == 0)
-									return (NULL);
-							while (i < size)
-									{
-												s[i] = c;
-														++i;
-															}
-								return (s);
+	if (size == 0)
+		return (NULL);
+	s = malloc(size * sizeof(char));
+	if (s == 0)
+		return (NULL);
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
 }
