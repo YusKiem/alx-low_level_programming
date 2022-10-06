1d48e6f54b9b7e8fc8ee7c829d442b8358b896c6
Yus Kiem <yuskiem@icloud.com>
2022-10-05 17:45:37 -0700

    out

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+
+#endif
